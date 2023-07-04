161e80e263b06348358621cbc39e59677250e0a3
Yusun Choi <nsuy.ch@gmail.com>
2023-07-04 21:24:55 +0900

    chore: Add BookmarkEntity, BookmarkRepository (#3)

---
+++ echo/src/main/java/com/echochamber/echo/domain/model/BookmarkEntity.java
@@ -0,0 +1,33 @@
+package com.echochamber.echo.domain.model;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@Table(name = "Bookmark")
+public class BookmarkEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    private UserEntity user;
+    @ManyToOne
+    private NewsEntity news;
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Builder
+    public BookmarkEntity(UserEntity user, NewsEntity news) {
+        this.user = user;
+        this.news = news;
+    }
+}
