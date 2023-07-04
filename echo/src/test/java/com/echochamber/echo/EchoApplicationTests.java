db9c3fd5e0e94c0e73064426c870434b7f837b58
Yusun Choi <nsuy.ch@gmail.com>
2023-07-04 16:11:05 +0900

    chore: Initialize spring project

---
+++ echo/src/test/java/com/echochamber/echo/EchoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.echochamber.echo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EchoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
