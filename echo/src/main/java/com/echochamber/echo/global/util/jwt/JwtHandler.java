8c425d952f04eb59a01041f1810c796365fd8579
Yusun Choi <nsuy.ch@gmail.com>
2023-07-06 00:35:54 +0900

    feat: Add JwtHandler (#3)

---
+++ echo/src/main/java/com/echochamber/echo/global/util/jwt/JwtHandler.java
@@ -0,0 +1,50 @@
+package com.echochamber.echo.global.util.jwt;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Header;
+import io.jsonwebtoken.InvalidClaimException;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.util.*;
+
+import static io.jsonwebtoken.Jwts.builder;
+import static io.jsonwebtoken.Jwts.parser;
+
+@Component
+public class JwtHandler {
+    private final String secretKey;
+
+    public JwtHandler(@Value("${JWT_SECRET_KEY}") String secretKey) {
+        this.secretKey = secretKey;
+    }
+
+    // 토큰 발행
+    public String generateToken(boolean isAccessToken, Map<String, Object> payloads) {
+        Date now = new Date();
+        Duration duration = isAccessToken ? Duration.ofHours(2) : Duration.ofDays(7);
+        Date expiration = new Date(now.getTime() + duration.toMillis());
+        String subject = isAccessToken ? "accessToken" : "refreshToken";
+
+        return builder().setHeaderParam(Header.TYPE, Header.JWT_TYPE)
+                .setClaims(payloads)
+                .setIssuer("admin")
+                .setIssuedAt(now)
+                .setExpiration(expiration)
+                .setSubject(subject)
+                .signWith(SignatureAlgorithm.HS256,
+                        Base64.getEncoder().encodeToString(secretKey.getBytes()))
+                .compact();
+    }
+
+    // 토큰 검증
+    public Map<String, Object> verifyJWT(String jwt)
+            throws InvalidClaimException, ExpiredJwtException {
+        return parser()
+                .setSigningKey(secretKey.getBytes())
+                .parseClaimsJws(jwt)
+                .getBody();
+    }
+}
