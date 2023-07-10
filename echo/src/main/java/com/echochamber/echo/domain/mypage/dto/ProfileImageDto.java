af9b5ba5cd537c99893f9c4804321d83b426bf68
Yusun Choi <nsuy.ch@gmail.com>
2023-07-10 22:15:54 +0900

    feat: Add ProfileImageDto (#7)

---
+++ echo/src/main/java/com/echochamber/echo/domain/mypage/dto/ProfileImageDto.java
@@ -0,0 +1,14 @@
+package com.echochamber.echo.domain.mypage.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+public class ProfileImageDto {
+    private final String profile_image_path;
+}
