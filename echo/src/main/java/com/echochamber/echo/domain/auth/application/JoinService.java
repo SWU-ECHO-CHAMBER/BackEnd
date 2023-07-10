ffe89902debd399218cd9aa4536c9239dd0741b7
Yusun Choi <nsuy.ch@gmail.com>
2023-07-10 22:15:07 +0900

    refactor: Move profile image handler on JoinService (#7)

--- echo/src/main/java/com/echochamber/echo/domain/auth/application/JoinService.java
+++ echo/src/main/java/com/echochamber/echo/domain/auth/application/JoinService.java
@@ -2,15 +2,14 @@ package com.echochamber.echo.domain.auth.application;
 
 import com.echochamber.echo.domain.auth.dao.UserRepository;
 import com.echochamber.echo.domain.model.UserEntity;
+import com.echochamber.echo.global.util.ImageHandler;
 import com.echochamber.echo.global.util.StringValidator;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
 import java.io.IOException;
 
 @Service
@@ -18,27 +17,18 @@ import java.io.IOException;
 public class JoinService {
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
-    private final String DIR_PATH;
+    private final ImageHandler imageHandler;
 
     @Autowired
-    public JoinService(PasswordEncoder passwordEncoder, UserRepository userRepository, @Value("${PROFILE_DATABASE_URL}") String path) {
+    public JoinService(PasswordEncoder passwordEncoder, UserRepository userRepository, ImageHandler imageHandler) {
         this.passwordEncoder = passwordEncoder;
         this.userRepository = userRepository;
-        this.DIR_PATH = path;
+        this.imageHandler = imageHandler;
     }
 
     // 비밀번호 암호화
     private String encodePassword(String password) {
         return passwordEncoder.encode(password);
-    }
-
-    // 프로필 이미지 저장
-    private String saveProfileImage(String email, MultipartFile image) throws IOException {
-        String file_name = email + ".jpeg";
-        String file_path = DIR_PATH + file_name;
-        image.transferTo(new File(file_path));
-
-        return file_path;
     }
 
     // 이메일 값 유효성 확인 및 중복확인
@@ -60,8 +50,13 @@ public class JoinService {
 
     // DB 정보 저장
     public Long saveData(String email, String nickname, String password, MultipartFile image) throws IOException {
+        // 비밀번호 암호화
         String encoded_password = encodePassword(password);
-        String profile_path = image == null || image.isEmpty() ? null : saveProfileImage(email, image);
+
+        // 프로필 이미지 저장
+        String profile_path = image == null || image.isEmpty() ? null : imageHandler.saveProfileImage(email, image);
+        
+        // 유저 객체 생성
         UserEntity newUser = new UserEntity(email, nickname, encoded_password, null, profile_path);
 
         return userRepository.save(newUser).getId();
